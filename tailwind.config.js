736a15e50c2cf9a7a3f0e402ec46047db02d6ffa
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2022-10-26 00:00:56 +0600

    finished

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1b4d89",
+          secondary: "#6db784",
+          accent: "#f9e45b",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
+
